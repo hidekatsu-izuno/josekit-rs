5bd139d93fbf1e3a04199fb59326178a9b81e249
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2020-08-08 15:50:55 +0900

    Fix a some bugs about multi signer.

--- src/jws/multi_signer.rs
+++ src/jws/multi_signer.rs
@@ -1,49 +1,93 @@
-use serde_json::{json, Map, Value};
+use anyhow::bail;
+use serde_json::{Map, Value};
 
 use crate::error::JoseError;
-use crate::jws::JwsSigner;
+use crate::jose::JoseHeader;
+use crate::jws::{JwsHeader, JwsSigner};
 
-pub struct JwsMultiSigner {
-    payload: String,
-    signatures: Vec<Value>,
+pub struct JwsMultiSigner<'a> {
+    signers: Vec<(Box<dyn JwsSigner + 'a>, Map<String, Value>)>,
 }
 
-impl JwsMultiSigner {
-    pub fn new(payload: &[u8]) -> Self {
+impl<'a> JwsMultiSigner<'a> {
+    pub fn new() -> Self {
         JwsMultiSigner {
-            payload: base64::encode_config(payload, base64::URL_SAFE_NO_PAD),
-            signatures: Vec::new(),
+            signers: Vec::new(),
         }
     }
 
-    pub fn add_signature(
+    pub fn add_signer(
         &mut self,
-        signer: &dyn JwsSigner,
-        protected_header: &Map<String, Value>,
-        unprotected_header: &Map<String, Value>,
+        signer: impl JwsSigner + 'a,
+        protected_header: Option<JwsHeader>,
+        unprotected_header: Option<JwsHeader>,
     ) -> Result<(), JoseError> {
-        let protected_header = serde_json::to_string(&protected_header).unwrap();
-        let protected_header = base64::encode_config(protected_header, base64::URL_SAFE_NO_PAD);
+        (|| -> anyhow::Result<()> {
+            let mut sig_item = Map::new();
 
-        let message = format!("{}.{}", protected_header, self.payload);
-        let signature = signer.sign(message.as_bytes())?;
-        let signature = base64::encode_config(&signature, base64::URL_SAFE_NO_PAD);
+            let mut protected_map;
+            let protected;
+            if let Some(val) = protected_header {
+                protected_map = val.claims_set().clone();
+                protected_map.insert("alg".to_string(), Value::String(signer.algorithm().name().to_string()));
 
-        self.signatures.push(json!({
-            "protected": protected_header,
-            "header": unprotected_header,
-            "signature": signature,
-        }));
+                let json = serde_json::to_string(&protected_map).unwrap();
+                protected = base64::encode_config(json, base64::URL_SAFE_NO_PAD);
+                sig_item.insert("protected".to_string(), Value::String(protected));
+            } else {
+                protected_map = Map::new();
+                protected_map.insert("alg".to_string(), Value::String(signer.algorithm().name().to_string()));
 
-        Ok(())
+                let json = serde_json::to_string(&protected_map).unwrap();
+                protected = base64::encode_config(json, base64::URL_SAFE_NO_PAD);
+                sig_item.insert("protected".to_string(), Value::String(protected));
+            }
+
+            if let Some(val) = unprotected_header {
+                let map = val.claims_set().clone();
+                for key in map.keys() {
+                    if protected_map.contains_key(key) {
+                        bail!("Duplicate key exists: {}", key);
+                    }
+                }
+
+                if map.len() > 0 {
+                    sig_item.insert("header".to_string(), Value::Object(map));
+                }
+            }
+
+            self.signers.push((
+                Box::new(signer),
+                sig_item,
+            ));
+
+            Ok(())
+        })()
+        .map_err(|err| match err.downcast::<JoseError>() {
+            Ok(err) => err,
+            Err(err) => JoseError::InvalidJwsFormat(err)
+        })
     }
 
-    pub fn serialize_json(&self) -> Result<String, JoseError> {
-        Ok(format!(
-            "{{\"payload\":{},\"signatures\":{}}}",
-            &serde_json::to_string(&self.payload).unwrap(),
-            &serde_json::to_string(&self.signatures).unwrap(),
-        ))
+    pub fn serialize_json(&self, payload: &[u8]) -> Result<String, JoseError> {
+        let payload = base64::encode_config(payload, base64::URL_SAFE_NO_PAD);
+
+        let mut sig_vec = Vec::new();
+        for (signer, sig_item) in &self.signers {
+            let protected = sig_item.get("protected").unwrap();
+            let message = format!("{}.{}", protected, payload);
+            let signature = signer.sign(message.as_bytes())?;
+            let signature = base64::encode_config(&signature, base64::URL_SAFE_NO_PAD);
+            
+            let mut sig_item = sig_item.clone();
+            sig_item.insert("signature".to_string(), Value::String(signature));
+            sig_vec.push(Value::Object(sig_item));
+        }
+
+        let signatures = serde_json::to_string(&sig_vec).unwrap();
+        let result = format!("{{\"signatures\":{},\"payload\":\"{}\"}}", &signatures, &payload);
+        
+        Ok(result)
     }
 }
 
@@ -51,21 +95,28 @@ impl JwsMultiSigner {
 mod tests {
     use super::*;
     use crate::jws::{RS256, ES256};
-/*
+    use crate::jwk::KeyPair;
+
     #[test]
-    fn sign_multpile() -> Result<()> {
+    fn sign_multpile() -> anyhow::Result<()> {
         let payload = b"abcde012345";
 
-        let mut multi_signer = JwsMultiSigner::new(payload);
+        let mut multi_signer = JwsMultiSigner::new();
 
         let keypair1 = RS256.generate_keypair(2048)?;
         let signer1 = RS256.signer_from_der(keypair1.to_der_private_key())?;
+        let protected1 = JwsHeader::new();
+        let header1 = JwsHeader::new();
+        multi_signer.add_signer(signer1, Some(protected1), Some(header1))?;
 
         let keypair2 = ES256.generate_keypair()?;
+        let signer2 = ES256.signer_from_der(keypair2.to_der_private_key())?;
+        let protected2 = JwsHeader::new();
+        let header2 = JwsHeader::new();
+        multi_signer.add_signer(signer2, Some(protected2), Some(header2))?;
 
-        multi_signer.add_signature(signer: &dyn JwsSigner, protected_header: &Map<String, Value>, unprotected_header: &Map<String, Value>)
+        println!("{}", multi_signer.serialize_json(payload)?);
 
         Ok(())
     }
-*/
 }
